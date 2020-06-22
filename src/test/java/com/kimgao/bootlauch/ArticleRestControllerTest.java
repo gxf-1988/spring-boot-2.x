9fe10869eeb4e544d249f717fae3800ccd8e9fe2
GXF <120276191@qq.com>
2020-06-22 15:50:48 +0800

    2-3-1 Mockito测试(最终版本)

--- src/test/java/com/kimgao/bootlauch/ArticleRestControllerTest.java
+++ src/test/java/com/kimgao/bootlauch/ArticleRestControllerTest.java
@@ -2,29 +2,28 @@ package com.kimgao.bootlauch;
 
 import com.kimgao.bootlauch.controller.ArticleController;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Slf4j
-class ArticleRestControllerTest {
-
-    //mock对象
+public class ArticleRestControllerTest {
+    //Mock对象
     private static MockMvc mockMvc;
 
-    //在所有测试方法执行之前进行mock对象初始化
     @BeforeAll
-    static void setUp() {
+    static void setUp(){
         mockMvc = MockMvcBuilders.standaloneSetup(new ArticleController()).build();
     }
 
-    //测试方法
     @Test
     public void saveArticle() throws Exception {
 
@@ -33,20 +32,24 @@ class ArticleRestControllerTest {
                 "    \"author\": \"zimug\",\n" +
                 "    \"title\": \"手摸手教你开发spring boot\",\n" +
                 "    \"content\": \"c\",\n" +
-                "    \"createTime\": \"2017-07-16 05:23:34\",\n" +
+                "    \"createTime\": \"2017/07/16 05:23:34\",\n" +
                 "    \"reader\":[{\"name\":\"zimug\",\"age\":18},{\"name\":\"kobe\",\"age\":37}]\n" +
                 "}";
-        MvcResult result = mockMvc.perform(
+        log.info(article);
+        MvcResult mvcResult = mockMvc.perform(
                 MockMvcRequestBuilders
-                        .request(HttpMethod.POST, "/rest/articles")
-                        .contentType("application/json")
-                        .content(article)
+                    .request(HttpMethod.POST,"/rest/articles")
+                    .contentType("application/json")
+                    .accept(MediaType.APPLICATION_JSON_UTF8)
+                    .content(article)
         )
-                .andDo(print())
-                .andReturn();
-
-        result.getResponse().setCharacterEncoding("UTF-8");
-        log.info(result.getResponse().getContentAsString());
-
+         .andExpect(MockMvcResultMatchers.status().isOk())  //HTTP:status 200
+         .andExpect(MockMvcResultMatchers.jsonPath("$.data.author").value("zimug"))
+         .andExpect(MockMvcResultMatchers.jsonPath("$.data.reader[0].age").value(18))
+         .andDo(print())
+         .andReturn();
+        //mvcResult.getResponse().setCharacterEncoding("UTF-8");
+        log.info(mvcResult.getResponse().getContentAsString());
     }
+
 }
--- src/test/java/com/kimgao/bootlauch/ArticleRestControllerTest2.java
+++ src/test/java/com/kimgao/bootlauch/ArticleRestControllerTest2.java
@@ -1,40 +1,42 @@
 package com.kimgao.bootlauch;
 
-import com.kimgao.bootlauch.controller.ArticleController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kimgao.bootlauch.model.Article;
+import com.kimgao.bootlauch.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.annotation.Resource;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @Slf4j
 @AutoConfigureMockMvc
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-class ArticleRestControllerTest1 {
+class ArticleRestControllerTest2 {
 
     //mock对象
     @Resource
     private  MockMvc mockMvc;
 
+    @MockBean
+    private ArticleService articleService;
+
     //在所有测试方法执行之前进行mock对象初始化
-    /*@BeforeAll
-    static void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleController()).build();
-    }
-*/
+
     //测试方法
     @Test
     public void saveArticle() throws Exception {
@@ -47,16 +49,25 @@ class ArticleRestControllerTest1 {
                 "    \"createTime\": \"2017/07/16 05:23:34\",\n" +
                 "    \"reader\":[{\"name\":\"zimug\",\"age\":18},{\"name\":\"kobe\",\"age\":37}]\n" +
                 "}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Article articleObj = objectMapper.readValue(article,Article.class);
+
+        log.info("articleObj:"+articleObj);
+
+        //打桩
+        when(articleService.saveArticle(articleObj)).thenReturn("ok");
+
         MvcResult result = mockMvc.perform(
-                MockMvcRequestBuilders
-                        .request(HttpMethod.POST, "/rest/articles")
+                MockMvcRequestBuilders.request(HttpMethod.POST, "/rest/articles")
                         .contentType("application/json")
                         .accept(MediaType.APPLICATION_JSON_UTF8)
-                        .content(article)
-        )
+                        .content(article))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.author").value("zimug"))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.data").value("ok"))
                 .andDo(print())
                 .andReturn();
-        
+
         //result.getResponse().setCharacterEncoding("UTF-8");
         log.info(result.getResponse().getContentAsString());
 
