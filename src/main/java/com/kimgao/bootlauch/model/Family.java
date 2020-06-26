9b0cb9d09cff76d2f9ffec947c5a1120f152647d
GXF <120276191@qq.com>
2020-06-26 16:56:54 +0800

    3-3 YAML绑定变量和属性值校验

--- src/main/java/com/kimgao/bootlauch/model/Family.java
+++ src/main/java/com/kimgao/bootlauch/model/Family.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @Component
 //当我们的类不属于各种归类的时候（不属于@Controller、@Services等的时候），我们就可以使用@Component来标注这个类
 @ConfigurationProperties(prefix = "family")
+@Validated
 public class Family {
     //@Value("${family.family-name}")
     private String familyName;
--- src/main/java/com/kimgao/bootlauch/model/Father.java
+++ src/main/java/com/kimgao/bootlauch/model/Father.java
@@ -2,8 +2,11 @@ package com.kimgao.bootlauch.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public class Father {
     private String name;
+    @Min(21)
     private Integer age;
 }
