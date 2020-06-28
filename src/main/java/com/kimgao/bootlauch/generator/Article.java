477ae2b11e2bf5541c11e40123750babfcc0fd66
GXF <120276191@qq.com>
2020-06-28 11:00:21 +0800

    4-2 mybatis generator自动生成代码

---
+++ src/main/java/com/kimgao/bootlauch/generator/Article.java
@@ -0,0 +1,58 @@
+package com.kimgao.bootlauch.generator;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Article implements Serializable {
+    private Long id;
+
+    private String author;
+
+    private String title;
+
+    private String content;
+
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author == null ? null : author.trim();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
