8db81c812a3100ceb974c0e93dd698dd06be35de
GXF <120276191@qq.com>
2020-06-28 11:00:21 +0800

    4-2 mybatis generator自动生成代码

--- src/main/java/com/kimgao/bootlauch/BootLauchApplication.java
+++ src/main/java/com/kimgao/bootlauch/BootLauchApplication.java
@@ -1,10 +1,12 @@
 package com.kimgao.bootlauch;
 
 import com.kimgao.bootlauch.model.LombokPOJO;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.kimgao.bootlauch.generator"})
 public class BootLauchApplication {
 
     public static void main(String[] args) {
