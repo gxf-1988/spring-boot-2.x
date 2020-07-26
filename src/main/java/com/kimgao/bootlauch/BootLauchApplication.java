a66a44b5e9170a66b2cdeee5df5237e040eacec4
gxf-1988 <120276191@qq.com>
2020-07-26 22:42:50 +0800

    6-1 servlet与filter与listener的实现

--- src/main/java/com/kimgao/bootlauch/BootLauchApplication.java
+++ src/main/java/com/kimgao/bootlauch/BootLauchApplication.java
@@ -4,9 +4,11 @@ import com.kimgao.bootlauch.model.LombokPOJO;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.kimgao.bootlauch.generator"})
+@ServletComponentScan
 public class BootLauchApplication {
 
     public static void main(String[] args) {
