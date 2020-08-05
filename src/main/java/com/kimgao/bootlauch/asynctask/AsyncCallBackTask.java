6bc4ee5005f2ffd5ff452a32114e6ba44aa8f8d0
gxf-1988 <120276191@qq.com>
2020-08-05 21:22:37 +0800

    10-2 为异步任务规划线程池

--- src/main/java/com/kimgao/bootlauch/asynctask/AsyncCallBackTask.java
+++ src/main/java/com/kimgao/bootlauch/asynctask/AsyncCallBackTask.java
@@ -8,21 +8,24 @@ import java.util.concurrent.Future;
 
 @Component
 public class AsyncCallBackTask extends AbstractTask {
-    @Async
+    @Async("taskExecutor")
     public Future<String> doTaskOneCallback() throws Exception {
         super.doTaskOne();
+        System.out.println("任务一，当前线程：" + Thread.currentThread().getName());
         return new AsyncResult<>("任务一完成");
     }
 
-    @Async
+    @Async("taskExecutor")
     public Future<String> doTaskTwoCallback() throws Exception {
         super.doTaskTwo();
+        System.out.println("任务二，当前线程：" + Thread.currentThread().getName());
         return new AsyncResult<>("任务二完成");
     }
 
-    @Async
+    @Async("taskExecutor")
     public Future<String> doTaskThreeCallback() throws Exception {
         super.doTaskThree();
+        System.out.println("任务三，当前线程：" + Thread.currentThread().getName());
         return new AsyncResult<>("任务三完成");
     }
 }
