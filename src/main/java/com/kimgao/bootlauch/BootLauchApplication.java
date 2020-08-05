8c0b75b31dc0f4afa3f1f8a1050c37a6aba3884b
gxf-1988 <120276191@qq.com>
2020-08-05 16:25:51 +0800

    10-1 实现Async异步任务

--- src/main/java/com/kimgao/bootlauch/BootLauchApplication.java
+++ src/main/java/com/kimgao/bootlauch/BootLauchApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.kimgao.bootlauch.generator"})
 @ServletComponentScan
+@EnableAsync
 public class BootLauchApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/kimgao/bootlauch/asynctask/AsyncCallBackTask.java
@@ -0,0 +1,28 @@
+package com.kimgao.bootlauch.asynctask;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Future;
+
+@Component
+public class AsyncCallBackTask extends AbstractTask {
+    @Async
+    public Future<String> doTaskOneCallback() throws Exception {
+        super.doTaskOne();
+        return new AsyncResult<>("任务一完成");
+    }
+
+    @Async
+    public Future<String> doTaskTwoCallback() throws Exception {
+        super.doTaskTwo();
+        return new AsyncResult<>("任务二完成");
+    }
+
+    @Async
+    public Future<String> doTaskThreeCallback() throws Exception {
+        super.doTaskThree();
+        return new AsyncResult<>("任务三完成");
+    }
+}
